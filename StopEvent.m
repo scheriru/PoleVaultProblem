a3464623c1b36a806e7ef1640a674e73ceb8300a
scheriru <sherylchau5@gmail.com>
2020-04-16 18:23:03 -0400

    Update StopEvent.m

--- StopEvent.m
+++ StopEvent.m
@@ -1,6 +1,6 @@
 function [value, isterminal, direction] = StopEvent(t,y)
 
-value      = [y(3); round(sqrt(y(1)^2+y(2)^2),3)-1];
-isterminal = [1;1];   % Stop the integration
-direction  = [0;1];
+value      = round(sqrt(y(1)^2+y(2)^2),3)-1;
+isterminal = 1;   % Stop the integration
+direction  = 1;
 end
